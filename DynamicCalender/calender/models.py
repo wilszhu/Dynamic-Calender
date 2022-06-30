0a9581de4d63cca6fcf83444a12dc126e2abac3c
HenryYang1228 <108202203+HenryYang1228@users.noreply.github.com>
2022-06-30 14:01:29 +0800

    Added the class named "events"

--- DynamicCalender/calender/models.py
+++ DynamicCalender/calender/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+class events(models.Model):
+    #The name of event
+    eventName=models.CharField(max_length=200)
+    #assignedDate #The time scheduled for the event
+    def __str__(self):
+        return self.eventName #Returns the name of event in string, human readable form
+    #def recentEvent #This function is to tell whether the evnet will occur recently
