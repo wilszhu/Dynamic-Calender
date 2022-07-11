713a8784d1c68bdd51c40c9f46c22afa010f163c
AlbertZheng123 <anpei.zheng@gmail.com>
2022-07-11 11:44:51 -0700

    Added/Changed views

--- DynamicCalender/calender/views.py
+++ DynamicCalender/calender/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-def feed(request):
-    return render(request, 'calender/feed.html')
+def feed_page(request):
+    return render(request, 'calender/templates/feed.html')
+
+def calendar_page(request):
+    return render(request, 'calendar/templates/calendar.html')
+
+def wellness(request):
+    return render(request, 'calendar/templates/wellness.html')
