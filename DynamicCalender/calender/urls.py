330af669af4ea1a041fd207a51779d5f8312bc74
wilmason <97467909+wilmason@users.noreply.github.com>
2022-07-25 23:26:14 -0400

    User Authentication

--- DynamicCalender/calender/urls.py
+++ DynamicCalender/calender/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='home/',
+                                  permanent=False)),
+    path('home/', views.home, name='home'),
     path('feed/', views.feed_page, name='feed'),
     path('calendar/', views.calendar_page, name = 'calendar'),
     path('wellness/', views.wellness, name = 'wellness'),\
--- DynamicCalender/calender/views.py
+++ DynamicCalender/calender/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Events, Calender
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 def feed_page(request):
@@ -9,3 +13,37 @@ def calendar_page(request):
 
 def wellness(request):
     return render(request, 'calendar/templates/wellness.html')
+
+def home(request):
+    return render(request, 'calendar/templates/home.html')
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'calendar/templates/home.html')
+        else:
+            return render(request, 'calendar/templates/login.html')
+    else:
+        return render(request, 'calendar/templates/login.html')
+
+def register_page(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return render(request, 'calendar/templates/home.html')
+    else:
+        form = UserCreationForm()
+    return render(request, 'calendar/templates/register.html', {'form': form})
+
+def logout_page(request):
+    logout(request)
+    return render(request, 'calendar/templates/login.html')
