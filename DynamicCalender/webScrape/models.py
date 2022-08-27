59f8da74e35549b64c019050026072068b1b0fb3
AlbertZheng123 <anpei.zheng@gmail.com>
2022-08-26 19:05:05 -0700

    Code for get links to science google news

--- DynamicCalender/webScrape/models.py
+++ DynamicCalender/webScrape/models.py
@@ -3,3 +3,13 @@ import requests
 from bs4 import BeautifulSoup
 # Create your models here.
 
+list_of_websites = ["https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFp0Y1RjU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen", "https://www.timeshighereducation.com/", "https://skift.com/"]
+
+def web_scrape(website):
+    url = website
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    div = soup.find("div", {"class": "lBwEZb BL5WZb GndZbb"})
+    for link in div:
+        print(link.find('a')['href'])
+        
